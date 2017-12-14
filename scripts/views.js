49e556c21df9761d18943084245163310c7fd14c
Richard Livingston <livingston.richard@gmail.com>
2017-12-14 14:21:55 -0800

    Added some routes, which half work, and the functions that the routes call half work as well. Will finish soon.

--- scripts/views.js
+++ scripts/views.js
@@ -1,5 +1,29 @@
-let gameView = {}; // eslint-disable-line
+'use strict';
 
-gameView.initSelectPage = function() {
-    
-};
+var app = app || {}; // eslint-disable-line
+
+
+(function (module) {
+    let gameView = {}; // eslint-disable-line
+
+    gameView.initSelectPage = function() {
+        $('main section').hide();
+        $('#selection').show();
+    };
+// ↓↓↓↓↓↓↓↓↓↓↓↓↓↓ Do we need this initGamePage?↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+    gameView.initGamePage = function() {
+        $('main section').hide();
+        $('#game-board').show();
+    };
+
+    gameView.initLeaderboardPage = function(){
+        $('main section').hide();
+        $('#leaderboard').show();
+    };
+
+    gameView.initAboutPage = function(){
+        $('main section').hide();
+        $('#about').show();
+    };
+    module.gameView = gameView;
+})(app);
